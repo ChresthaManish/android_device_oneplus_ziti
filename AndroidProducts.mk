4742fc48c571cdb00255d674cab406a19acf2e05
Manish Shrestha <Shresthamanish6067@gmail.com>
2025-04-23 18:53:25 +0545

    Revert "lemonade: Drop COMMON_LUNCH_CHOICES"
    This reverts commit 716fc2793abc65937ef7bf11bfca0ad9195d5a0a.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,14 @@
 #
-# Copyright (C) 2021-2024 The LineageOS Project
+# Copyright (C) 2021-2024 The Lineag eOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_ziti.mk
+    
+COMMON_LUNCH_CHOICES := \
+    aosp_ziti-user \
+    aosp_ziti-userdebug \
+    aosp_ziti-eng
+
